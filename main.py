34db7db67f8263c97b5817b5ee264e4fe4b17c23
“Vadim <vadiml@wizardsdev.com>
2021-04-09 09:37:32 +0300

    Improved logs

--- main.py
+++ main.py
@@ -25,31 +25,25 @@ def iri_to_uri(iri):
 @bot.message_handler(content_types=['text'])
 def get_reddit_content(message):
      if "https://www.reddit.com/" in message.text:
-        try:
-            print("Chat title: " + message.chat.title)
-        except Exception as e:
-            print("Chat id:" + str(message.chat.id))
+        chat_identity = message.chat.title if message.chat.id < 0 else str(message.chat.id)
+        print("Chat identity: " + chat_identity)
+
         now = datetime.now()
         current_time = now.strftime("%d/%m/%Y %H:%M:%S")
         url_message = message.text
         start_url = url_message.find("https")
         url = iri_to_uri(url_message[start_url:])
         try:
-            retry_count = 0
+            retry_count = 1
             url_response = ...
             while(retry_count<=10):
                 try:
+                    time.sleep(1)
                     url_response = urllib.request.urlopen(url)
                     break
                 except Exception as e:
+                    bot.send_message('-556187948', "Chat identity: " + chat_identity + '\n' + "Retry - " + str(retry_count))
                     retry_count += 1
-                    time.sleep(1)
-                    try:
-                        bot.send_message('-556187948',
-                                         'Group tittle: ' + message.chat.title + '\n' + "Retry - " + str(retry_count))
-                    except Exception as e:
-                        bot.send_message('-556187948', 'Chat id: ' +  str(message.chat.id) + '\n' + "Retry - " + str(retry_count))
-                    continue
             response_data = url_response.read().decode('utf-8')
             tittle = response_data[response_data.find('<title>') + 7:response_data.find('</title>')]
             tittle = tittle.replace("&#x27;","'").replace("&quot;","")
@@ -62,14 +56,14 @@ def get_reddit_content(message):
                 foto = open('local-filename.jpg', 'rb')
                 os.remove("local-filename.jpg")
                 bot.send_media_group(message.chat.id, [InputMediaPhoto(foto, tittle[0:tittle.find(':')])], None, message.id)
-                print('Success: "type":"image"')
+                print(current_time + ' Success: "type":"image"')
             elif '"type":"gifvideo"' in response_data:
                 arr = response_data.split('"type":"gifvideo"')
                 sub = arr[0][-200:]
                 draft_url = sub[sub.find('https://'):]
                 url = draft_url[:draft_url.find('"')]
                 bot.reply_to(message, tittle[0:tittle.find(':')] + '\n' + url.replace("\\u0026", "&"))
-                print('Success: "type":"gifvideo"')
+                print(current_time + ' Success: "type":"gifvideo"')
             elif '.mp4' in response_data:
                 arr = response_data.split('.mp4')
                 sub = arr[0]
@@ -78,15 +72,15 @@ def get_reddit_content(message):
                 try:
                     urllib.request.urlopen(sub[-39:-2] + resolution + '.mp4')
                     bot.reply_to(message, tittle[0:tittle.find(':')] + '\n' + sub[-39:-2] + resolution + '.mp4')
-                    print('Success: ".mp4"')
+                    print(current_time + ' Success: ".mp4"')
                 except Exception as e:
                     bot.reply_to(message, tittle[0:tittle.find(':')] + '\n' + sub[-39:-2] + '240.mp4')
-                    print('Success: "240.mp4"')
+                    print(current_time + ' Success: "240.mp4"')
             elif 'https://i.imgur.com/' in response_data:
                 draft_url = response_data[response_data.find('https://i.imgur.com/'):]
                 url = draft_url[:draft_url.find('"')-4]
                 bot.reply_to(message, tittle[0:tittle.find(':')] + '\n' + url + 'mp4')
-                print('Success: "https://i.imgur.com/"')
+                print(current_time + ' Success: "https://i.imgur.com/"')
             elif 'class="_3BxRNDoASi9FbGX01ewiLg' in response_data:
                 arr = response_data.split('_3BxRNDoASi9FbGX01ewiLg')
                 img_arr = []
@@ -103,7 +97,7 @@ def get_reddit_content(message):
                             img_arr.append(InputMediaPhoto(foto))
                         os.remove("local-filename."+ image_dimension)
                 bot.send_media_group(message.chat.id, img_arr, None, message.id)
-                print('Success: "class="_3BxRNDoASi9FbGX01ewiLg"')
+                print(current_time + ' Success: "class="_3BxRNDoASi9FbGX01ewiLg"')
             elif 'class="_3spkFGVnKMHZ83pDAhW3Mx' in response_data:
                 arr = response_data.split('class="_3spkFGVnKMHZ83pDAhW3Mx')
                 img_arr = []
@@ -119,7 +113,7 @@ def get_reddit_content(message):
                             img_arr.append(InputMediaPhoto(foto))
                         os.remove("local-filename.jpg")
                 bot.send_media_group(message.chat.id, img_arr, None, message.id)
-                print('Success: "class="_3spkFGVnKMHZ83pDAhW3Mx"')
+                print(current_time + ' Success: "class="_3spkFGVnKMHZ83pDAhW3Mx"')
             elif '<meta property="og:type" content="image" />' in response_data:
                 point = response_data.find('property="og:image"')
                 start_url = response_data.find("https://", point)
@@ -130,22 +124,19 @@ def get_reddit_content(message):
                 os.remove("local-filename.jpg")
                 bot.send_media_group(message.chat.id, [InputMediaPhoto(foto, tittle[0:tittle.find(':')])], None,
                                      message.id)
-                print('Success: "property="og:image""')
+                print(current_time + ' Success: "property="og:image""')
             else:
                 file = open("logs_fails.txt", "a")
                 file.write(current_time + '\n' + str(message.chat.id) + '\n' + url + '\n' + '\n')
                 file.close()
                 bot.reply_to(message, "image not found")
-                print("image not found")
+                print(current_time + ' "image not found"')
         except Exception as e:
-            try:
-                bot.send_message('-556187948', 'Group tittle: ' + message.chat.title + '\n' + 'Error: ' + format(e))
-            except Exception as e:
-                bot.send_message('-556187948', 'Chat id: ' + str(message.chat.id) + '\n' + 'Error: ' + format(e))
+            bot.send_message('-556187948', "Chat identity: " + chat_identity + '\n' + 'Error: ' + format(e))
             file = open("logs_errors.txt", "a")
             file.write(current_time + '\n' + str(message.chat.id) + '\n' + url + '\n' + format(e) + '\n' + '\n')
             file.close()
             bot.reply_to(message, "something went wrong")
-            print("something went wrong" + '\n' + format(e))
+            print(current_time + ' something went wrong' + '\n' + format(e))
 
 bot.polling(none_stop=True)
