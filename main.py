f24d20424e2818299254152f0781c45528ee9506
“Vadim <vadiml@wizardsdev.com>
2021-07-19 10:46:28 +0300

    Improved media_goup handler

--- main.py
+++ main.py
@@ -22,17 +22,19 @@ def iri_to_uri(iri):
     ))
     return uri
 
+
 def get_current_time():
     now = datetime.now()
     return now.strftime("%d/%m/%Y %H:%M:%S")
 
+
 @bot.message_handler(content_types=['text'])
 def get_reddit_content(message):
-     if "https://www.reddit.com/" in message.text:
+    if "https://www.reddit.com/" in message.text:
         chat_identity = message.chat.title if message.chat.id < 0 else str(message.chat.id)
         chat_identity = "!!!ME!!!" if chat_identity == "153550168" else chat_identity
         chat_identity = "!!!RUSIK!!!" if chat_identity == "258744216" else chat_identity
-
+        chat_identity = "!!!MAX!!!" if chat_identity == "391339107" else chat_identity
 
         try:
             file = open("id.txt", "r")
@@ -46,7 +48,8 @@ def get_reddit_content(message):
         file = open("id.txt", "w")
         file.write(str(id + 1))
         file.close()
-        print('-------------------------' + '\n' + get_current_time() + " id: " + str(id) + " Chat identity: " + chat_identity)
+        print('-------------------------' + '\n' + get_current_time() + " id: " + str(
+            id) + " Chat identity: " + chat_identity)
         url_message = message.text
         start_url = url_message.find("https")
         url = iri_to_uri(url_message[start_url:])
@@ -61,44 +64,30 @@ def get_reddit_content(message):
         try:
             retry_count = 1
             url_response = ...
-            while(retry_count<=10):
+            while (retry_count <= 10):
                 try:
                     time.sleep(0.1)
                     url_response = urllib.request.urlopen(req)
                     break
                 except Exception as e:
-                    bot.send_message('-556187948', "Chat identity: " + chat_identity + '\n' + "Retry - " + str(retry_count))
+                    bot.send_message('-556187948',
+                                     "Chat identity: " + chat_identity + '\n' + "Retry - " + str(retry_count))
                     retry_count += 1
             response_data = url_response.read().decode('utf-8')
             tittle = response_data[response_data.find('<title>') + 7:response_data.find('</title>')]
-            tittle = tittle.replace("&#x27;","'").replace("&quot;","")
+            tittle = tittle.replace("&#x27;", "'").replace("&quot;", "")
             if '"type":"image"' in response_data:
                 point = response_data.find('"type":"image"')
-                start_url = response_data.find("https://", point-100)
+                start_url = response_data.find("https://", point - 100)
                 end_url = response_data.find('"', start_url)
                 draft_url = response_data[start_url: end_url]
                 time.sleep(0.1)
-                urllib.request.urlretrieve(draft_url.replace("\\u0026", "&"), str(id)+".jpg")
+                urllib.request.urlretrieve(draft_url.replace("\\u0026", "&"), str(id) + ".jpg")
                 foto = open(str(id) + '.jpg', 'rb')
-                os.remove(str(id)+".jpg")
-                bot.send_media_group(message.chat.id, [InputMediaPhoto(foto, tittle[0:tittle.find(':')])], None, message.id)
+                os.remove(str(id) + ".jpg")
+                bot.send_media_group(message.chat.id, [InputMediaPhoto(foto, tittle[0:tittle.find(':')])], None,
+                                     message.id)
                 print(get_current_time() + " id: " + str(id) + ' Success: "type":"image"')
-            elif '"type":"gifvideo"' in response_data:
-                arr = response_data.split('"type":"gifvideo"')
-                sub = arr[0][-200:]
-                draft_url = sub[sub.find('https://'):]
-                url = draft_url[:draft_url.find('"')]
-                bot.reply_to(message, tittle[0:tittle.find(':')] + '\n' + url.replace("\\u0026", "&"))
-                print(get_current_time() + " id: " + str(id) + ' Success: "type":"gifvideo"')
-            elif 'https://gfycat.com/' in response_data:
-                draft_url = response_data[response_data.find('https://gfycat.com/'):]
-                inner_url = draft_url[:draft_url.find('"')]
-                inner_url_response = urllib.request.urlopen(inner_url)
-                inner_response_data = inner_url_response.read().decode('utf-8')
-                draft_url = inner_response_data[inner_response_data.find("og:video:secure_url")+30:]
-                url = draft_url[:draft_url.find('"')]
-                bot.reply_to(message, tittle[0:tittle.find(':')] + '\n' + url)
-                print(get_current_time() + " id: " + str(id) + ' Success: "https://gfycat.com/"')
             elif ('.mp4' in response_data) & ('.mp4,' not in response_data):
                 arr = response_data.split('DASH_96.mp4"')
                 sub = arr[0]
@@ -127,9 +116,9 @@ def get_reddit_content(message):
                             url_for_combine = 'https://ds.redditsave.com/download-sd.php?permalink=' + url + '/&video_url=' + sub[
                                                                                                                               -32:] + 'DASH_240.mp4' + '&audio_url=' + sub[
                                                                                                                                                                        -32:] + 'DASH_audio.mp4'
-                    urllib.request.urlretrieve(url_for_combine, str(id)+".mp4")
+                    urllib.request.urlretrieve(url_for_combine, str(id) + ".mp4")
                     video = open(str(id) + '.mp4', 'rb')
-                    os.remove(str(id)+".mp4")
+                    os.remove(str(id) + ".mp4")
                     bot.send_media_group(message.chat.id, [InputMediaVideo(video, None, tittle[0:tittle.find(':')])],
                                          None, message.id)
                     print(get_current_time() + " id: " + str(id) + ' Success: ".mp4" with sound')
@@ -145,7 +134,24 @@ def get_reddit_content(message):
                         urllib.request.urlopen(sub[-32:] + 'DASH_240.mp4')
                         bot.reply_to(message, tittle[0:tittle.find(':')] + '\n' + sub[-32:] + 'DASH_240.mp4')
                         print(get_current_time() + " id: " + str(id) + ' Success: "240.mp4"')
-            elif 'class="_3BxRNDoASi9FbGX01ewiLg' in response_data:
+            elif 'https://gfycat.com/' in response_data:
+                draft_url = response_data[response_data.find('https://gfycat.com/'):]
+                inner_url = draft_url[:draft_url.find('"')]
+                inner_url_response = urllib.request.urlopen(inner_url)
+                inner_response_data = inner_url_response.read().decode('utf-8')
+                draft_url = inner_response_data[inner_response_data.find("og:video:secure_url") + 30:]
+                url = draft_url[:draft_url.find('"')]
+                bot.reply_to(message, tittle[0:tittle.find(':')] + '\n' + url)
+                print(get_current_time() + " id: " + str(id) + ' Success: "https://gfycat.com/"')
+            elif '"type":"gifvideo"' in response_data:
+                arr = response_data.split('"type":"gifvideo"')
+                sub = arr[0][-200:]
+                draft_url = sub[sub.find('https://'):]
+                url = draft_url[:draft_url.find('"')]
+                bot.reply_to(message, tittle[0:tittle.find(':')] + '\n' + url.replace("\\u0026", "&"))
+                print(get_current_time() + " id: " + str(id) + ' Success: "type":"gifvideo"')
+            elif 'class="_3BxRNDoASi9FbGX01ewiLg' in response_data or 'class="_3spkFGVnKMHZ83pDAhW3Mx' in response_data:
+                separator = '_3BxRNDoASi9FbGX01ewiLg' if 'class="_3BxRNDoASi9FbGX01ewiLg' in response_data else '_3spkFGVnKMHZ83pDAhW3Mx'
                 arr = response_data.split('_3BxRNDoASi9FbGX01ewiLg')
                 img_arr = []
                 img_count = 0
@@ -155,10 +161,10 @@ def get_reddit_content(message):
                     if 'href="https://preview.redd.it/' in sub:
                         draft_url = sub[sub.find('https://preview.redd.it/'):]
                         url = draft_url[:draft_url.find('"')]
-                        image_dimension = url[url.find("?")-3:url.find("?")]
+                        image_dimension = url[url.find("?") - 3:url.find("?")]
                         time.sleep(0.1)
-                        urllib.request.urlretrieve(url.replace('amp;', ''), str(id)+"."+ image_dimension)
-                        foto = open(str(id) + '.'+ image_dimension, 'rb')
+                        urllib.request.urlretrieve(url.replace('amp;', ''), str(id) + "." + image_dimension)
+                        foto = open(str(id) + '.' + image_dimension, 'rb')
                         if len(img_arr) == 0:
                             if len(arr) > 21:
                                 part_count += 1
@@ -166,51 +172,24 @@ def get_reddit_content(message):
                             img_arr.append(InputMediaPhoto(foto, tittle[0:tittle.find(':')] + part_string))
                         else:
                             img_arr.append(InputMediaPhoto(foto))
-                        os.remove(str(id)+"."+ image_dimension)
+                        os.remove(str(id) + "." + image_dimension)
                         img_count += 1
-                        if img_count == 9:
+                        if img_count == 10:
                             bot.send_media_group(message.chat.id, img_arr, None, message.id)
                             img_arr = []
                             img_count = 0
-                bot.send_media_group(message.chat.id, img_arr, None, message.id)
-                print(get_current_time() + " id: " + str(id) + ' Success: "class="_3BxRNDoASi9FbGX01ewiLg"')
-            elif 'class="_3spkFGVnKMHZ83pDAhW3Mx' in response_data:
-                arr = response_data.split('class="_3spkFGVnKMHZ83pDAhW3Mx')
-                img_arr = []
-                img_count = 0
-                part_string = ""
-                part_count = 0
-                for sub in arr:
-                    if 'href="https://preview.redd.it/' in sub:
-                        draft_url = sub[sub.find('https://preview.redd.it/'):]
-                        url = draft_url[:draft_url.find('"')]
-                        time.sleep(0.1)
-                        urllib.request.urlretrieve(url.replace('amp;', ''), str(id)+".jpg")
-                        foto = open(str(id) + '.jpg', 'rb')
-                        if len(img_arr) == 0:
-                            if len(arr) > 20:
-                                part_count += 1
-                                part_string = " (Part " + str(part_count) + ")"
-                            img_arr.append(InputMediaPhoto(foto, tittle[0:tittle.find(':')] + part_string))
-                        else:
-                            img_arr.append(InputMediaPhoto(foto))
-                        os.remove(str(id)+".jpg")
-                        img_count += 1
-                        if img_count == 9:
-                            bot.send_media_group(message.chat.id, img_arr, None, message.id)
-                            img_arr = []
-                            img_count = 0
-                bot.send_media_group(message.chat.id, img_arr, None, message.id)
-                print(get_current_time() + " id: " + str(id) + ' Success: "class="_3spkFGVnKMHZ83pDAhW3Mx"')
+                if img_count % 10 != 0:
+                    bot.send_media_group(message.chat.id, img_arr, None, message.id)
+                print(get_current_time() + " id: " + str(id) + ' Success: ' + separator)
             elif '<meta property="og:type" content="image" />' in response_data:
                 point = response_data.find('property="og:image"')
                 start_url = response_data.find("https://", point)
                 end_url = response_data.find('"', start_url)
                 draft_url = response_data[start_url: end_url]
                 time.sleep(0.1)
-                urllib.request.urlretrieve(draft_url.replace("amp;", ''), str(id)+".jpg")
+                urllib.request.urlretrieve(draft_url.replace("amp;", ''), str(id) + ".jpg")
                 foto = open(str(id) + '.jpg', 'rb')
-                os.remove(str(id)+".jpg")
+                os.remove(str(id) + ".jpg")
                 bot.send_media_group(message.chat.id, [InputMediaPhoto(foto, tittle[0:tittle.find(':')])], None,
                                      message.id)
                 print(get_current_time() + " id: " + str(id) + ' Success: "property="og:image""')
@@ -254,21 +233,24 @@ def get_reddit_content(message):
                 print(get_current_time() + " id: " + str(id) + ' Success: YouTube')
             elif 'https://i.imgur.com/' in response_data:
                 draft_url = response_data[response_data.find('https://i.imgur.com/'):]
-                url = draft_url[:draft_url.find('"')-4]
+                url = draft_url[:draft_url.find('"') - 4]
                 bot.reply_to(message, tittle[0:tittle.find(':')] + '\n' + url + 'mp4')
                 print(get_current_time() + " id: " + str(id) + ' Success: "https://i.imgur.com/"')
             else:
                 file = open("logs_fails.txt", "a")
-                file.write(get_current_time() + " id: " + str(id) + '\n' + str(message.chat.id) + '\n' + url + '\n' + '\n')
+                file.write(
+                    get_current_time() + " id: " + str(id) + '\n' + str(message.chat.id) + '\n' + url + '\n' + '\n')
                 file.close()
                 bot.reply_to(message, "image not found")
                 print(get_current_time() + " id: " + str(id) + ' "image not found"')
         except Exception as e:
             bot.send_message('-556187948', "Chat identity: " + chat_identity + '\n' + 'Error: ' + str(e))
             file = open("logs_errors.txt", "a")
-            file.write(get_current_time() + " id: " + str(id) + '\n' + str(message.chat.id) + '\n' + url + '\n' + str(e) + '\n' + '\n')
+            file.write(get_current_time() + " id: " + str(id) + '\n' + str(message.chat.id) + '\n' + url + '\n' + str(
+                e) + '\n' + '\n')
             file.close()
             bot.reply_to(message, "something went wrong")
             print(get_current_time() + " id: " + str(id) + ' something went wrong' + '\n' + str(e))
+
 
 bot.polling(none_stop=True)
